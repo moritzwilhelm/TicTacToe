fb87364f196adb493797259334964c0cc1ef0d1b
Moritz Wilhelm <s8mowilh@stud.uni-saarland.de>
2019-12-12 02:41:12 +0100

    introduced player class

--- tictactoe.py
+++ tictactoe.py
@@ -1,68 +1,53 @@
-import os
-
-
 class TicTacToeBoard:
-    def __init__(self, player1, player2):
-        self.board = [[None, None, None], [None, None, None], [None, None, None]]
-        self.player1 = player1
-        self.player2 = player2
-        self.current_player = player1
+    def __init__(self):
+        self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
-    def switch_current_player(self):
-        self.current_player = self.player2 if self.current_player == self.player1 else self.player1
-
-    def perform_move(self, pos):
-        self.board[pos // 3][pos % 3] = self.current_player == self.player1
+    def perform_move(self, pos, player):
+        self.board[pos // 3][pos % 3] = player.symbol
 
     def is_full(self):
-        return None not in (self.board[0] + self.board[1] + self.board[2])
+        return ' ' not in (self.board[0] + self.board[1] + self.board[2])
 
     def move_possible(self, pos):
         if not self.is_valid_move(pos):
-            print("This space is already marked. Please select another one.")
+            print('This space is already marked. Please select another one.')
             return False
         else:
             return True
 
     def is_valid_move(self, pos):
-        return self.board[pos // 3][pos % 3] is None
+        return self.board[pos // 3][pos % 3] is ' '
 
     def check_rows(self):
         for i in range(3):
-            if self.board[i][0] == self.board[i][1] == self.board[i][2] and self.board[i][0] is not None:
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] and self.board[i][0] is not ' ':
                 return True
         return False
 
     def check_columns(self):
         for i in range(3):
-            if self.board[0][i] == self.board[1][i] == self.board[2][i] and self.board[0][i] is not None:
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] and self.board[0][i] is not ' ':
                 return True
         return False
 
     def check_diagonals(self):
-        return self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[1][1] is not None or \
-               self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[1][1] is not None
+        return self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[1][1] is not ' ' or \
+               self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[1][1] is not ' '
 
-    def check_game_status(self):
+    def check_game_status(self, player):
         if self.check_rows() or self.check_columns() or self.check_diagonals():
-            print("Congratulations! %s wins." % self.current_player)
+            print('Congratulations! %s wins.' % player.name)
             self.print_board()
             exit(0)
-        self.switch_current_player()
 
     def print_board(self, with_id=False):
-        items = []
+        symbols = []
         for line in reversed(self.board):
-            for item in line:
-                if item is None:
-                    items.append(" ")
-                elif item:
-                    items.append("X")
-                elif not item:
-                    items.append("O")
+            for symbol in line:
+                symbols.append(symbol)
         if with_id:
-            items = list('789456123')
-        print("""
+            symbols = list('789456123')
+        print('''
         ###########
         ##%s##%s##%s##
         ###########
@@ -70,11 +55,16 @@ class TicTacToeBoard:
         ###########
         ##%s##%s##%s##
         ###########
-        """ % tuple(items))
+        ''' % tuple(symbols))
 
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+
+    def get_move(self):
+        return int(input('%s, please enter the ID of the space you want to mark: ' % self.name)) - 1
 
 
 def main():
@@ -82,26 +72,32 @@ def main():
     print('Enter the corresponding ID in order to mark a space.')
     print('Hint: Use the numpad to have an easy ID mapping.\n')
 
-    player1 = input("Player1, please enter your name: ")
-    player2 = input("Player2, please enter your name: ")
+    player1_name = input('Player1, please enter your name: ')
+    player2_name = input('Player2, please enter your name: ')
 
-    board = TicTacToeBoard(player1, player2)
+    player1 = Player(player1_name, 'X')
+    player2 = Player(player2_name, 'O')
+
+    board = TicTacToeBoard()
     board.print_board(with_id=True)
+
+    current_player = player1
 
     while not board.is_full():
         try:
-            choice = int(input("%s, please insert the ID of the space you want to mark: " % board.current_player)) - 1
+            choice = current_player.get_move()
             if choice < 0 or choice > 8:
                 raise ValueError
         except ValueError:
-            print("Please enter a digit between 1-9")
+            print('Please enter a digit between 1-9')
             continue
         if board.move_possible(choice):
-            board.perform_move(choice)
-            board.check_game_status()
+            board.perform_move(choice, current_player)
+            board.check_game_status(current_player)
             board.print_board()
+            current_player = player2 if current_player == player1 else player1
 
-    print("It's a tie!")
+    print('It\'s a tie!')
 
 
 if __name__ == '__main__':
