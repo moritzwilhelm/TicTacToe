c430f8f130108eab17613101c006f0fc0049681a
Moritz Wilhelm <s8mowilh@stud.uni-saarland.de>
2019-12-12 02:03:32 +0100

    Update tictactoe.py
    + renamed grid to borad
    + added TicTacToeBoard.is_full()
    + added TicTacToeBoard.is_valid_move()
    + moved input validation to main (and TicTacToeBoard.is_valid_move())
    
    ! crucial BugFix in TicTacToeBoard.check_diagonals()

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,9 @@
 import os
 
 
-class TicTacToeGrid():
+class TicTacToeBoard:
     def __init__(self, player1, player2):
-        self.grid = [[None, None, None], [None, None, None], [None, None, None]]
+        self.board = [[None, None, None], [None, None, None], [None, None, None]]
         self.free_spaces = 9
         self.player1 = player1
         self.player2 = player2
@@ -12,53 +12,49 @@ class TicTacToeGrid():
     def switch_current_player(self):
         self.current_player = self.player2 if self.current_player == self.player1 else self.player1
 
-    def mark_space(self, position):
-        pos = int(position) - 1
-        self.grid[pos // 3][pos % 3] = self.current_player == self.player1
+    def mark_space(self, pos):
+        self.board[pos // 3][pos % 3] = self.current_player == self.player1
         self.free_spaces -= 1
 
-    def move_possible(self, position=None):
-        if self.free_spaces == 0:
+    def is_full(self):
+        return self.free_spaces == 0
+
+    def move_possible(self, pos):
+        if not self.is_valid_move(pos):
+            print("This space is already marked. Please select another one.")
             return False
-        if position is None:
+        else:
             return True
-        try:
-            position = int(position) - 1
-            assert position in range(9)
-            field_status = self.grid[position // 3][position % 3] is None
-            if not field_status:
-                print("This space is already marked. Please select another one.")
-            return field_status
-        except (ValueError, AssertionError):
-            print("Please enter a digit between 1-9")
-            return False
+
+    def is_valid_move(self, pos):
+        return self.board[pos // 3][pos % 3] is None
 
     def check_rows(self):
         for i in range(3):
-            if self.grid[i][0] == self.grid[i][1] == self.grid[i][2] and self.grid[i][0] is not None:
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] and self.board[i][0] is not None:
                 return True
         return False
 
     def check_columns(self):
         for i in range(3):
-            if self.grid[0][i] == self.grid[1][i] == self.grid[2][i] and self.grid[0][i] is not None:
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] and self.board[0][i] is not None:
                 return True
         return False
 
     def check_diagonals(self):
-        return self.grid[0][0] == self.grid[1][1] == self.grid[2][2] or \
-               self.grid[0][2] == self.grid[1][1] == self.grid[2][0] and self.grid[1][1] is not None
+        return self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[1][1] is not None or \
+               self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[1][1] is not None
 
     def check_game_status(self):
         if self.check_rows() or self.check_columns() or self.check_diagonals():
             print("Congratulations! %s wins." % self.current_player)
-            self.print_grid()
+            self.print_board()
             exit(0)
         self.switch_current_player()
 
-    def print_grid(self, with_id=False):
+    def print_board(self, with_id=False):
         items = []
-        for line in reversed(self.grid):
+        for line in reversed(self.board):
             for item in line:
                 if item is None:
                     items.append(" ")
@@ -91,16 +87,21 @@ def main():
     player1 = input("Player1, please enter your name: ")
     player2 = input("Player2, please enter your name: ")
 
-    game = TicTacToeGrid(player1, player2)
-    game.print_grid(with_id=True)
+    board = TicTacToeBoard(player1, player2)
+    board.print_board(with_id=True)
 
-    while game.move_possible():
-        choice = input("%s, please insert the ID of the space you want to mark: " % game.current_player)
-
-        if game.move_possible(choice):
-            game.mark_space(choice)
-            game.check_game_status()
-            game.print_grid()
+    while not board.is_full():
+        try:
+            choice = int(input("%s, please insert the ID of the space you want to mark: " % board.current_player)) - 1
+            if choice < 0 or choice > 8:
+                raise ValueError
+        except ValueError:
+            print("Please enter a digit between 1-9")
+            continue
+        if board.move_possible(choice):
+            board.mark_space(choice)
+            board.check_game_status()
+            board.print_board()
 
     print("It's a tie!")
 
